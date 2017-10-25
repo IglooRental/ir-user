0c7626b6126a58213fb9c09fd3a3cc37bcc3717b
Sašo Stanovnik <sstanovnik@gmail.com>
2017-10-25 10:46:14 +0200

    Initial example.

---
+++ src/main/java/si/uni_lj/fri/rso/ir_user/CustomerApplication.java
@@ -0,0 +1,8 @@
+package si.uni_lj.fri.rso.ir_user;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class CustomerApplication extends Application {
+}
